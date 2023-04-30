b70bef2af175d083a34d35745f7525cd0f728653
nccharles <nccharles1@gmail.com>
2023-04-30 11:15:49 +0200

    started a default route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+import express from 'express';
+
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to sms and email notification service!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
